75dd48e46e5134c86e05250cc6f9f3aef96f446a
Gleb Kosilov <allyxan1337@gmail.com>
2024-05-28 00:54:37 +0300

    #7

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,4 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig({ base: "/ar_ring_try-on", plugins: [react()], }) 
