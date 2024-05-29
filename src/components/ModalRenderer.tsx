41b910039bbdcbf52fadbb83da41c14ca7f21e4c
Gleb Kosilov <allyxan1337@gmail.com>
2024-05-30 02:53:25 +0300

    no cdn ok ok

--- src/components/ModalRenderer.tsx
+++ src/components/ModalRenderer.tsx
@@ -3,8 +3,7 @@ import { useEffect, useRef, useState } from 'react'
 import * as THREE from 'three';
 import { Carousel } from 'react-responsive-carousel';
 import { HandLandmarker, FilesetResolver } from "@mediapipe/tasks-vision";
-// import { drawConnectors, drawLandmarks } from '@mediapipe/drawing_utils';
-import { drawConnectors, drawLandmarks } from 'https://cdn.jsdelivr.net/npm/@mediapipe/drawing_utils@0.10.0';
+import { drawConnectors, drawLandmarks } from '@mediapipe/drawing_utils';
 import { HAND_CONNECTIONS } from '@mediapipe/holistic';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { ringModelAtom } from '../atomStorage';
