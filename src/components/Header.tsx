fe3db9269ff646a088e68fb58211b119dad97c69
Gleb Kosilov <allyxan1337@gmail.com>
2024-05-29 03:18:33 +0300

    UPD #11

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <Flex flexDirection="column" alignItems="center" bg="gray.600" boxShadow="md" w="100%" p={4} mb={10}>
       <Heading size="md" color="white">
-        Примерка колец с применением дополненной реальности v7
+        Примерка колец с применением дополненной реальности v8
       </Heading>
     </Flex>
   );
